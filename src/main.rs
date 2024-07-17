b1f482c1aeb40e75fec09251671f0e90b447dca9
Tim Burks <timburks@me.com>
2024-07-16 20:21:37 -0700

    Call a gRPC service with TLS

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use tonic::{transport::Channel, Request};
-
 use bobadojo_apis::bobadojo::stores::v1::stores_client::StoresClient;
 use bobadojo_apis::bobadojo::stores::v1::ListStoresRequest;
+use tonic::transport::{Certificate, Channel, ClientTlsConfig};
+use tonic::Request;
 
-const ENDPOINT: &str = "http://localhost:8080";
+const ENDPOINT: &str = "https://stores-ryzkosl3jq-uw.a.run.app:443";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = Channel::from_static(ENDPOINT).connect().await?;
+    let data_dir = std::path::PathBuf::from_iter(["data"]);
+    let pem = std::fs::read_to_string(data_dir.join("tls/ca.pem"))?;
+    let ca = Certificate::from_pem(pem);
+
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(ca)
+        .domain_name("googleapis.com");
+
+    let channel = Channel::from_static(ENDPOINT)
+        .tls_config(tls)?
+        .connect()
+        .await?;
+
     let mut service = StoresClient::with_interceptor(channel, move |req: Request<()>| Ok(req));
     let response = service
         .list_stores(Request::new(ListStoresRequest {
